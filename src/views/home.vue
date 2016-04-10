5b8ae9c0ef5a30bd22e92de320a142b4ec53373d
xiangshoulai <xiangshoulai@126.com>
2016-04-10 14:21:06 +0800

    shouye

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class='viewports'>
+    	<div class='container home-pannel'>
+    		<div class='floor-item>
+    			<div class="home-menu">
+    				<span class="menu-icon"></span>
+    				<span class="menu-tag">全部商品</span>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+</template>
+<style>
+	.home-pannel .home-menu{
+		height:44px;
+		width:100%;
+	}
+</style>
